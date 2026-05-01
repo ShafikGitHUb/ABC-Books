fe717eacdea975a4b8224a7d68099c7a303f36a3
ShafikGitHUb <princeshafik11@gmail.com>
2026-05-01 21:22:53 +0600

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/share/home/Navbar";
+import Footer from "@/components/share/home/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const PoppinsFont = Poppins({
+  variable: "--font-Poppins",
+  weight: ["100","200","300","400","500","600","700","800","900"],
   subsets: ["latin"],
 });
 
@@ -20,9 +19,14 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      data-theme="light"      
+      className={`${PoppinsFont.className} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body>
+        <Navbar></Navbar>
+        {children}
+        <Footer></Footer>
+        </body>
     </html>
   );
 }
